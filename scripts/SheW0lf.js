00a443af59f9c7c857fd233ae98b163685667d70
Amber Cyr <>
2018-10-03 08:15:54 -0400

    fixed the missing if condition

--- scripts/SheW0lf.js
+++ scripts/SheW0lf.js
@@ -7,6 +7,7 @@ for(i = 1; i <= 100; i++){
       if(i % 5 === 0){
         console.log('spiders');
       }
+    }else if(i % 3 === 0){
       console.log('cats');
     }else if(i % 5 === 0){
       console.log('bats');
