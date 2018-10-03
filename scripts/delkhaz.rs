ada438059be7aa1238048ddc73400b37c10324ab
Delkhaz <Delkhazibrahimi@gmail.com>
2018-10-03 12:17:31 -0500

    Rust Cats,Bats and Spiders

---
+++ scripts/delkhaz.rs
@@ -0,0 +1,13 @@
+fn main() {
+    for i in 1..101 {
+        if i % 3 == 0 && i % 5 == 0 {
+            println!("spider");
+        }else if i % 3 == 0 {
+            println!("cats");
+        }else if i % 5 == 0 {
+            println!("bats");
+        }else {
+            println!("{}", i);
+        }
+    }
+}
