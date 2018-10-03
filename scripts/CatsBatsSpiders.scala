30e38f32ab1c673b995de4fdd556c6710c3dbaff
Damian Dabrowski <dabek1812@gmail.com>
2018-10-03 17:15:50 +0200

    Scala

---
+++ scripts/CatsBatsSpiders.scala
@@ -0,0 +1,20 @@
+object CatsBatsSpiders {
+   def main(args: Array[String]) {
+      var i = 0;
+
+      // for loop execution with a range
+      for( i <- 1 to 100){
+         if (i % 3 == 0) {
+             if (i % 5 == 0 ) {
+                 println("Spiders")
+             } else {
+                 println("Cats")
+             }
+         } else if (i % 5 == 0) {
+             println("Bats")
+         } else {
+             println(i)
+         }
+      }
+   }
+}
