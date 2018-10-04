30f151cc699d306c00e01115bf6213cad821bef9
Shubham Bhandari <dev.sb18@gmail.com>
2018-10-04 14:45:27 +0530

    Added solution in Java and Python

---
+++ scripts/shubham.py
@@ -0,0 +1,13 @@
+# Author: Shubham Bhandari
+# Language: Python 3.7
+# Github: https://github.com/dev-SB
+
+for i in range(1, 101):
+    if i % 15 is 0:
+        print('spiders')
+    elif i % 3 is 0:
+        print('cats')
+    elif i % 5 is 0:
+        print('bats')
+    else:
+        print(i)
