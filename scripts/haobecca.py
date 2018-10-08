91d8ee594ca42b8dbc715aff3cf7793e653cb354
haobecca <rebeccaanneh@mail.usf.edu>
2018-10-07 19:15:55 -0500

    Added my python solution

---
+++ scripts/haobecca.py
@@ -0,0 +1,16 @@
+'''
+  Author: Becca Hao
+  Language: Python
+  Github: haobecca
+
+'''
+
+for i in range (1, 101):
+    if i % 15 == 0:
+        print('spiders')
+    elif i % 3 == 0:
+        print('cats')
+    elif i % 5 == 0:
+        print('bats')
+    else:
+        print(i)
