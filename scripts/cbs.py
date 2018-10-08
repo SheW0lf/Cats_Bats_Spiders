1140542bd9db25c3958c920444db09612fec417e
taufikalgi <43858377+taufikalgi@users.noreply.github.com>
2018-10-08 20:00:46 +0700

    Add files via upload

--- scripts/cbs.py
+++ scripts/cbs.py
@@ -1,13 +1,9 @@
-#Author: Anish 
-#Language: Python
-#Github: https://github.com/anish03
-
-for i in range(1,101):
-    if i % 3 == 0:
-        print 'cats'
-    elif i % 5 == 0:
-        print 'bats'
-    elif i % 3 == 0 and i % 5 == 0:
-        print 'spiders'
-    else:
-        print i
+for i in range (1,101):
+	if (i % 15 == 0):
+		print("Spider")
+	elif (i % 5 == 0):
+		print("bats")
+	elif (i % 3 == 0):
+		print("cats")
+	else:
+		print(i)		
