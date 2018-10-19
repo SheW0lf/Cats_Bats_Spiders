cafbcdf3e2bb2b6f8078ae7f5359877f8317804f
sarcoma <sarcoma@live.co.uk>
2018-10-19 22:10:40 +0100

    Reordered lambdas

--- scripts/sarcoma.py
+++ scripts/sarcoma.py
@@ -6,7 +6,7 @@ Github: https://github.com/sarcoma
 
 
 def main():
-    print('\n'.join([(lambda i: 'spiders' if i % 15 == 0 else (lambda i: 'cats' if i % 3 == 0 else (lambda i: 'bats' if i % 5 == 0 else str(i))(i))(i))(i) for i in range(1, 101)]))
+    print('\n'.join([(lambda i: 'cats' if i % 3 == 0 else (lambda i: 'bats' if i % 5 == 0 else (lambda i: 'spiders' if i % 15 == 0 else str(i))(i))(i))(i) for i in range(1, 101)]))
 
 
 if __name__ == '__main__':
